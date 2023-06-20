e1d23aba448f8dff5bac9670bfccb3c6fd73e8e6
vieiragranzoto <vieiragranzoto@gmail.com>
2023-06-20 19:34:42 -0300

    Refactor packages

--- src/main/java/med/voll/api/domain/medico/MedicoRepository.java
+++ src/main/java/med/voll/api/domain/medico/MedicoRepository.java
@@ -1,11 +1,11 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
 
-    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
+    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
     
 }
